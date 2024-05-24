650db9a9d6da50e33338afb437f7007945efa78c
dstmath <dstmath@163.com>
2024-05-24 15:19:13 +0800

    Update otp_insensor_dev.c

--- drivers/misc/mediatek/cam_cal/src/mt6833/otp_insensor_dev.c
+++ drivers/misc/mediatek/cam_cal/src/mt6833/otp_insensor_dev.c
@@ -410,7 +410,7 @@ int hi556_read_data(u16 addr, u8 *data)
 	last_addr = addr;
 	return read_reg16_data8(0x0108, data);
 }
-void hi556_otp_read_enable()
+void hi556_otp_read_enable(void)
 {
 	write_reg16_data8(0x0a02, 0x01);
 	write_reg16_data8(0x0a00, 0x00);
@@ -423,7 +423,7 @@ void hi556_otp_read_enable()
 	write_reg16_data8(0x003e, 0x10);
 	write_reg16_data8(0x0a00, 0x01);
 }
-void hi556_otp_read_disable()
+void hi556_otp_read_disable(void)
 {
 	write_reg16_data8(0x0a00, 0x00);
 	mdelay(10);
@@ -505,7 +505,7 @@ int hi846_read_data(u16 addr, u8 *data)
 	return read_reg16_data8(0x0708, data);
 }
 
-void hi846_otp_read_enable()
+void hi846_otp_read_enable(void)
 {
 	write_reg16_data8(0x0A02, 0x01);
 	write_reg16_data8(0x0A00, 0x00);
@@ -519,7 +519,7 @@ void hi846_otp_read_enable()
 	write_reg16_data8(0x0A00, 0x01);
 }
 
-void hi846_otp_read_disable()
+void hi846_otp_read_disable(void)
 {
 	write_reg16_data8(0x0a00, 0x00);
 	mdelay(10);
